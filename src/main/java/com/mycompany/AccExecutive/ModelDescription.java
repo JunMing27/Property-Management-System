39dea7480d4f43237f0b58a4473e24db663d890a
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-03-02 20:37:44 +0800

    delete

--- src/main/java/com/mycompany/AccExecutive/ModelDescription.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.AccExecutive;
-
-public interface ModelDescription {
-    public abstract void fromString(String line);
-    public abstract String toString();
-}
--- src/main/java/com/mycompany/BuildingExecutive/ModelDescription.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.BuildingExecutive;
-
-public interface ModelDescription {
-    public void fromString(String line);
-    public String toString();
-}
